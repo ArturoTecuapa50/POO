1fcdd68c62a1498035eb54503f8a77f5aac96d2b
ArturoTecuapa50 <54416941+ArturoTecuapa50@users.noreply.github.com>
2019-09-10 12:36:47 -0700

    Add files via upload

---
+++ practica2/practica2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace practica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+            lista.Add("hola");
+            lista.Add("hey");
+            lista.Add("yes");
+            foreach(string s in lista)
+            Console.WriteLine(s);
+            
+            
+            
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
