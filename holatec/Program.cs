326d0802416a02359bef930a6ebaa17c8604869c
ArturoTecuapa50 <54416941+ArturoTecuapa50@users.noreply.github.com>
2019-09-10 17:18:39 -0700

    Add files via upload

---
+++ holatec/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace holatec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA TEC");
+        }
+    }
+}
