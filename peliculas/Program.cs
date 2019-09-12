0fb585aa1ac197f1569533d1a4d2c3364ef80265
ArturoTecuapa50 <54416941+ArturoTecuapa50@users.noreply.github.com>
2019-09-11 20:23:44 -0700

    Update and rename peliculas/Program.cs to  peliculas/Program.cs

--- peliculas/Program.cs
+++ peliculas/Program.cs
@@ -39,20 +39,11 @@ namespace peliculas
     {
         static void Main(string[] args)
         {
-            peliculas = peli1 new peliculas();
-            peli1.titulo = "El Renacido" ;
-            peli1.año = 2016 ;
-            peli1.pais = "Estados Unidos";
-            peli1.director = "Alejandro González Iñárritu";
-            
-            peliculas = peli2 new peliculas();
-            peli2.titulo = "Black Panther" ;
-            peli2.año = 2018 ;
-            peli2.pais = "Estados Unidos";
-            peli2.director = "Ryan Coogler";
-            
-            Console.WriteLine("Peliculas ganadora a un Oscar: titulo: {0}, año: {1}",peli1.titulo, peli1.año);
-            Console.WriteLine("Peliculas ganadora a un Oscar: titulo: {0}, año: {1}",peli2.titulo, peli2.año);
+            peliculas peli1 = new peliculas();
+            peli1.setTitulo("Black Panther");
+            peli1.setAño(2018);
+            Console.WriteLine("{0}, ({1})" , peli1.getTitulo(), peli1.getAño());
+        
         }
     }
 }
