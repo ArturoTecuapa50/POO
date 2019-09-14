6bf6eca5cb5df4e5842b9dd1137e22c759db0575
ArturoTecuapa50 <54416941+ArturoTecuapa50@users.noreply.github.com>
2019-09-14 14:12:32 -0700

    Add files via upload

---
+++ listapeliculas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace listapeliculas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+            lista.Add("Hercules");
+            lista.Add("Los increibles");
+            lista.Add("Los aristogatos");
+            lista.Add("El libro de la selva");
+            lista.Add("Oliver y su pandilla");
+
+            foreach(string peliculas in lista)
+            Console.WriteLine(peliculas);
+            
+        }
+    }
+}
