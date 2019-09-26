9f8bcddc5e5ed9f8f1f19e760ba33f0f3f471975
ArturoTecuapa50 <arturo.tecuapa19@tectijuana.edu.mx>
2019-09-25 19:24:49 -0700

    cambio

---
+++ espias/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace espias
+{
+
+    class Persona
+    {
+        public string nombre;
+        public string apellido;
+
+        public Persona()
+        {
+            nombre = "jose";
+            apellido = "pavon";
+        }
+    }
+
+class Anonymous {
+   public static void anonimiza( Persona p)
+    {
+            p.nombre = "xxxxxxxxxxx";
+            p.apellido = "xxxxxxxxxxx";       
+    }
+
+    public static void cambia(ref Persona p )
+      {
+          p = new Persona();
+          p.nombre = "new";
+      }
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Persona p = new Persona();
+         Console.WriteLine(p.nombre);
+         Anonymous.anonimiza( p);
+         Anonymous.cambia(ref p);
+         Console.WriteLine(p.nombre);
+         
+
+        }
+    }
+}
