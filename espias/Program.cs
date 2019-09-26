229475a42b8be8e1c94926b31545972d368fc4be
ArturoTecuapa50 <arturo.tecuapa19@tectijuana.edu.mx>
2019-09-25 19:35:03 -0700

    cambio

--- espias/Program.cs
+++ espias/Program.cs
@@ -31,13 +31,28 @@ class Anonymous {
 
     class Program
     {
+        static void duplicar(ref int x){
+            x=x*2;
+        }
+        static int suma(int x,int y){
+            return x+y;
+        } 
+         static void suma(int x,int y, out int resultado){
+            resultado = x+y;
+        } 
         static void Main(string[] args)
         {
-         Persona p = new Persona();
+            int a=2;
+            int b =3;
+            int r= suma(a,b);
+            int r2;
+            suma(a,b,out r2);
+            Console.WriteLine(r2);
+         /* Persona p = new Persona();
          Console.WriteLine(p.nombre);
          Anonymous.anonimiza( p);
          Anonymous.cambia(ref p);
-         Console.WriteLine(p.nombre);
+         Console.WriteLine(p.nombre);*/
          
 
         }
