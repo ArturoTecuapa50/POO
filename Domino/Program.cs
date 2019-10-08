0ca1bad384a67e1c2860016688587bcbd58b6f42
ArturoTecuapa50 <54416941+ArturoTecuapa50@users.noreply.github.com>
2019-10-08 16:43:55 -0700

    primera

---
+++ Domino/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Domino
+{
+    class Domino{
+        public int a;
+        public int b;
+
+
+        public domino(int a, int b){
+            this.a=a;
+            this.b=b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            domino a= new domino(2,0);
+            domino b= new domino(4,1);
+            Console.WriteLine(a+b);
+        }
+    }
+}
