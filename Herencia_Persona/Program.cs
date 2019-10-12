b419f8b2335b912b08d8d09ce5f7a75555038aa4
ArturoTecuapa50 <arturo.tecuapa19@tectijuana.edu.mx>
2019-10-11 19:36:27 -0700

    nuevo

---
+++ Herencia_Persona/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Herencia_Persona
+{
+    class Persona{
+        private string _nombre;
+
+        public string Nombre{
+            get {
+                //return _nombre <---- cuando es privada
+                return _nombre;
+            }
+        }
+        public Persona(string n){
+            _nombre=n;
+        }
+        public void print(){
+            Console.WriteLine("Mi nombre es {0}",_nombre);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona tom = new Persona("Tom");
+            Persona jim = new Persona("Jim");
+            tom.print();
+            jim.print();
+        }
+    }
+}
