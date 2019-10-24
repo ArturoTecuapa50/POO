78026e8006c318eb1c2b5e6d515d7d3ae95e3be2
ArturoTecuapa50 <54416941+ArturoTecuapa50@users.noreply.github.com>
2019-10-23 19:12:07 -0700

    cambios

---
+++ Alumnos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Alumnos
+{
+    class Alumnos{
+        protected string nombre;
+        protected string no_control;
+        
+        public Alumnos(string n, string nocontrol){
+            nombre=n;
+            no_control=nocontrol;      
+
+        }
+        public virtual string datos_alumno(){
+            return string.Format("Su Nombres es: {0}, su No.Control es : {1}",nombre,no_control);
+        }
+        
+    }
+    class Licenciatira:Alumnos{
+            private string Li;
+            public Licenciatira(string nombre, string no_control):base(nombre,no_control){
+                this.Li=Li;
+            }
+            public override string datos_alumno(){
+                return base.datos_alumno()+", hago servicio social y recidencia ";
+            }    
+    }
+    class Posgrado:Alumnos{
+        private string pos;
+        public Posgrado(string nombre, string no_control):base(nombre,no_control){
+                this.pos=pos;
+            }
+            public override string datos_alumno(){
+                return base.datos_alumno()+", tengo un tema de investigacion ";
+            }  
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Licenciatira p= new Licenciatira("Jose","12345");
+            Posgrado l= new Posgrado ("Javier","01231");
+            
+            Console.WriteLine(p.datos_alumno());
+            Console.WriteLine(l.datos_alumno());
+        }
+    }
+}
