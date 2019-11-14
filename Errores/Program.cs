c818a682baf4c3d28078531ae2eabf2693db04b3
ArturoTecuapa50 <arturo.tecuapa19@tectijuana.edu.mx>
2019-11-13 20:45:15 -0800

    cambio

--- Errores/Program.cs
+++ Errores/Program.cs
@@ -8,10 +8,21 @@ namespace Errores
         {
             int valor;
             Console.WriteLine("ingresa tu edad: ");
-            string captura=Console.ReadLine();
-            valor =int.Parse(captura);
-            Console.WriteLine("En diez años tendras {0}", 100/valor);
-            Console.ReadKey();
+            //string captura=Console.ReadLine();
+            //valor =int.Parse(captura);
+            //Console.WriteLine("En diez años tendras {0}", 100/valor);
+            //Console.ReadKey();
+            try
+            {
+                string captura=Console.ReadLine();
+                valor =int.Parse(captura);
+                Console.WriteLine(valor);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("formato incorrecto");
+            }
+           
         }
     }
 }
