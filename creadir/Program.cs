601a8eb36060331edaacbcea3a4f6fc711aff65c
ArturoTecuapa50 <arturo.tecuapa19@tectijuana.edu.mx>
2019-11-19 12:56:40 -0800

    cambio

--- creadir/Program.cs
+++ creadir/Program.cs
@@ -6,16 +6,11 @@ namespace creadir
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("capturar el folder a crear");
+            Console.WriteLine("capturar el archivo a borrar");
 
-            string directorio=Console.ReadLine();
-
-            Directory.CreateDirectory(directorio);
-            if (Directory.Exists(directorio)){
-                Console.WriteLine("ya existe directorio");
-            }else{
-                Directory.CreateDirectory(directorio);
-            }
+            string archivo=Console.ReadLine();           
+            File.SetAttributes(archivo,FileAttributes.Hidden);
+            
            
         }
     }
