95f33aa55fe57b088de6701993fe71c807023bad
ArturoTecuapa50 <arturo.tecuapa19@tectijuana.edu.mx>
2019-11-19 12:26:34 -0800

    cambios

---
+++ creadir/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+namespace creadir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("capturar el folder a crear");
+
+            String directorio=Console.ReadLine();
+
+            Directory.CreateDirectory(directorio);
+           
+        }
+    }
+}
