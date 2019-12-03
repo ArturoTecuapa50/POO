2cf4151f185ce0bde3706ac6d8e19458316a2a41
ArturoTecuapa50 <arturo.tecuapa19@tectijuana.edu.mx>
2019-12-03 12:16:12 -0800

    agrege

---
+++ proyecto_final/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace proyecto_final
+{
+    class producto{
+        public string codigo;
+        public string descripcion;
+        public double precio;
+        public int departamento;
+        public int likes;
+        public producto(string c, string des, double p, int dep, int l){
+            codigo=c;
+            descripcion=des;
+            precio=p;
+            departamento=dep;
+            likes=l;
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
