cc96f83de474a72b16487b4230c525b602ca2c9d
ArturoTecuapa50 <arturo.tecuapa19@tectijuana.edu.mx>
2019-12-10 20:57:39 -0800

    cambios

--- proyecto_final/Program.cs
+++ proyecto_final/Program.cs
@@ -8,7 +8,7 @@ namespace proyecto_final
 	{
 		public string codigo;
 		public string descripcion;
-		public Decimal precio;
+		public decimal precio;
 		public int departamento;
 		public int likes;
 		public Producto(string c,string desc,decimal p, int dep, int l)
@@ -19,15 +19,16 @@ namespace proyecto_final
 			departamento=dep;
 			likes=l;
 		}
+		
 	}
 	class ProductoArchivo
 	{
 		public List<Producto> productos = new List<Producto>();
-
 		public void EscribeProductosTXT(string archivo){
 			try
 			{
-				FileStream fs=new FileStream(archivo,FileMode.OpenOrCreate,FileAccess.Write);	//crea archivo
+				List<Producto> productos = new List<Producto>();
+				FileStream fs=new FileStream(archivo,FileMode.OpenOrCreate,FileAccess.Write);	//clase que abre archivo
 				using(StreamWriter txtOut=new StreamWriter(fs)){//transcribe
 					foreach(Producto p in productos){
 						txtOut.WriteLine("{0}|{1}|{2}|{3}|{4}",p.codigo,p.descripcion,p.precio,p.departamento,p.likes);
@@ -39,16 +40,17 @@ namespace proyecto_final
                 Console.WriteLine(e.Message);
             }
             finally{
-                Console.WriteLine("");
+                Console.WriteLine("Finally");
             }	
 		}
 		public void LeerProductosTXT(string archivo){
 			try
-			{			
-				FileStream fs1=new FileStream(archivo,FileMode.Open,FileAccess.Read);//crea
+			{
+				List<Producto> productos = new List<Producto>();
+				FileStream fs1=new FileStream(archivo,FileMode.Open,FileAccess.Read);//clase que abre archivo
 				using(StreamReader txtOut=new StreamReader(fs1)){//transcribe
 					string line;				
-					while((line = txtOut.ReadLine()) != null){//para que valla checando renglon x renglon
+					while((line = txtOut.ReadLine()) != null){//para que valla checando del primero hasta el ultiomo renglon
 						string[] columnas = line.Split("|");//para dividir el constructor
 						productos.Add(new Producto(columnas[0], columnas[1], decimal.Parse(columnas[2]), int.Parse(columnas[3]), int.Parse(columnas[4])));
 					}
@@ -59,14 +61,15 @@ namespace proyecto_final
                 Console.WriteLine(e.Message);
             }
             finally{
-                Console.WriteLine("");
+                Console.WriteLine("Finally");
             }	
 		}
 		public void EscribeProductosBIN(string archivo){
 			try
 			{
-				FileStream fs=new FileStream(archivo,FileMode.OpenOrCreate,FileAccess.Write);
-				using(BinaryWriter binOut=new BinaryWriter(fs)){
+				List<Producto> productos = new List<Producto>();
+				FileStream fs=new FileStream(archivo,FileMode.OpenOrCreate,FileAccess.Write);//clase que abre archivo
+				using(BinaryWriter binOut=new BinaryWriter(fs)){//transcribe
 					foreach(Producto p in productos){
 						binOut.Write(p.codigo);
 						binOut.Write(p.descripcion);
@@ -81,15 +84,16 @@ namespace proyecto_final
                 Console.WriteLine(e.Message);
             }
             finally{
-                Console.WriteLine("");
+                Console.WriteLine("Finally");
             }	
 		}
 		public void LeerProductosBIN(string archivo){
 			try
 			{
+				List<Producto> productos = new List<Producto>();
 				FileStream fs1=new FileStream(archivo,FileMode.Open,FileAccess.Read);
 				using(BinaryReader binIn=new BinaryReader(fs1)) {
-					while(binIn.PeekChar() != -1){
+					while(binIn.PeekChar() != -1){//checa los caracteres
 						productos.Add(new Producto(binIn.ReadString(), binIn.ReadString(), binIn.ReadDecimal(), binIn.ReadInt32(), binIn.ReadInt32()));
 					}
 				}
@@ -99,7 +103,7 @@ namespace proyecto_final
                 Console.WriteLine(e.Message);
             }
             finally{
-                Console.WriteLine("");
+                Console.WriteLine("Finally");
             }	
 		}
 		public void GetDepartment(int Depto){
@@ -111,21 +115,26 @@ namespace proyecto_final
 			foreach(Producto p in q) {
 				Console.WriteLine("{0},{1},{2},{3},{4}",p.codigo,p.descripcion,p.precio,p.departamento,p.likes);
 			}
+		}		
+		public void imprime(){
+			foreach(Producto p in productos){
+                Console.WriteLine("{0},|{1}|{2}|{3}|{4}",p.codigo,p.descripcion,p.precio,p.departamento,p.likes);
+            }
 		}
 	}
 	class Program
 	{
 		static void Main(string[] args)
-		{
+		{			
 			ProductoArchivo o=new ProductoArchivo();
 			o.productos.Add(new Producto("werdfd","jabon",10,2,50));
 			o.productos.Add(new Producto("dfsdgs","sabritas",15,3,38));
 			o.productos.Add(new Producto("jgfxmd","pantalon",200,4,12));
 			o.productos.Add(new Producto("sdnjey","shampoo",40,2,60));
+            o.imprime();
 			o.EscribeProductosTXT("productos.txt");
 			o.EscribeProductosBIN("productos.bin");
-            o.GetDepartment(2);
-			
+            o.GetDepartment(2);			
 		}
 	}
 }
